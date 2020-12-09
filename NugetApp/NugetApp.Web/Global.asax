e84340cd2cd68af26127bcfa86a536f658000c4f
Yeasin Arafat <yeasinarafat10@gmail.com>
2020-12-09 19:24:55 +0600

    Added initial setup for project

---
+++ NugetApp/NugetApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NugetApp.Web.MvcApplication" Language="C#" %>
---
+++ NugetApp/NugetApp.Web/Global.asax.cs
@@ -0,0 +1,67 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace NugetApp.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RegisterAutofac();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void RegisterAutofac()
+        {
+            //var builder = new ContainerBuilder();
+            //builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            //builder.RegisterSource(new ViewRegistrationSource());
+
+            //// manual registration of types;
+            ////builder.RegisterType<LoggerService>.As<ILoggerService>();
+            ////builder.RegisterType<UnitOfWork>.As<IUnitOfWork>();
+            ////builder.RegisterType<ApplicationDbContext>();
+
+            //// For property injection using Autofac
+            //// builder.RegisterType<QuoteService>().PropertiesAutowired();
+
+            //var container = builder.Build();
+
+            //DependencyResolver.SetResolver(container);
+
+
+
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            //builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+
+
+            //// OPTIONAL: Register model binders that require DI.
+            //builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            //builder.RegisterModelBinderProvider();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterAssemblyModules(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
